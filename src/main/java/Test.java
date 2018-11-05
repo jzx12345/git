b24c4f4b059f20102c9c6ea97a22629b81974da1
zhansan <123456>
2018-11-05 20:34:40 +0800

    把本地工程提交

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,4 +2,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("ahahhggjaj");
     }
+    public void test(){
+        System.out.println("zengjiabufen");
+    }
 }
